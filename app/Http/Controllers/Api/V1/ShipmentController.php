c6b9698f714abd999720f006ef13cb5b23433d71
Nicolas <nicolashilarion420@gmail.com>
2022-11-09 20:52:58 -0500

    working in index and show methods in shipments controller

--- app/Http/Controllers/Api/V1/ShipmentController.php
+++ app/Http/Controllers/Api/V1/ShipmentController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Guide;
+use App\Models\StatusGuide;
 use Illuminate\Http\Request;
 
 class ShipmentController extends Controller
 {
-
+    
     /**
      * Display a listing of the resource.
      *
@@ -19,14 +20,66 @@ class ShipmentController extends Controller
         try {
             $shipments = new Guide();
 
-            return response()->json([
-                'data' => [
-                    'mis_envios' => $shipments->all()
-                ]
-            ]);
+                return response()->json([
+                    'data' => [
+                        'mis_envios' => $shipments->all([   
+                            'id_sucursal', 
+                            'status_id',
+                            'peso', 
+                            'largo', 
+                            'ancho', 
+                            'alto', 
+                            'aplica_contrapago',
+                            'valor_declarado',
+                            'created_at',
+                            'id_cliente'
+                        ]),
+                    ]
+                ]);
+            
+
+
         } catch (\Throwable $th) {
             throw $th;
         }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Guide $shipment)
+    {
+        try {
+            return response()->json([
+                'data' => [
+                    'mis_envios' => [
+                        'seguimiento' => $shipment->id_sucursal,
+                        'estado' => $shipment->status->name,
+                        'destino' => [
+                            $shipment->receiver->nombre, 
+                            $shipment->receiver->direccion
+                        ],
+                        'envio' => [
+                            $shipment->peso, 
+                            $shipment->largo, 
+                            $shipment->alto, 
+                            $shipment->ancho],
+                        'aplica contrapago' => $shipment->aplica_contrapago,
+                        'guia valor' => $shipment->valor_declarado,
+                        'usuario' => [
+                            $shipment->created_at, 
+                            $shipment->id_cliente
+                        ],
+                    ],
+                ]
+            ]);   
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
     }
 
     /**
