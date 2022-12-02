9f1fa4ccde97f1ee0e8f0b5cf15749b63a8b2f0c
Nicolas <nicolashilarion420@gmail.com>
2022-12-02 13:22:00 -0500

    go work in barcode for pdf

--- app/Http/Controllers/Api/V1/GuideMassiveExcelController.php
+++ app/Http/Controllers/Api/V1/GuideMassiveExcelController.php
@@ -15,49 +15,13 @@ class GuideMassiveExcelController extends Controller
     public function createMassiveGuides() {
         
         try {
-            $mg = (new CollectionGuidesImport)->toCollection(request()->file('file'));
-            
             /* Deberia convertir esta collection en los parametros de la colleccion misma */
-            /* AACA QUEDO Y VOY CON Novelty para CULMINAR */
-
-            if ($mg) {
-                //$guide = new Guide();
-                $receiver = new Receiver();
-
-                $collections = (new CollectionGuidesImport());
-                $collections->collection($mg);
-                //$collection->collection->row;
-                
-                    
-                    $guide = $collections;
-
-                    //$guide = new Guide();
-                    //$mg->collection();
+            (new CollectionGuidesImport)->import(request()->file('file'));
         
-                        $pdf = PDF::loadView('massive_pdf.generate', [
-                            /* 'valor_declarado' => (integer)['valordeclarado'],
-                            'aplica_contrapago' =>['aplicacontrapago'],
-                            'peso_bruto' => (integer)['pesobruto'],
-                            'unidad' =>['unidad'],
-                            'dice_contener' =>['dicecontener'],
-                            'observaciones' =>['observaciones'],
-                            'peso' => (integer)['peso'],
-                            'largo' => (integer)['largo'],
-                            'ancho' => (integer)['ancho'],
-                            'alto' => (integer)['alto'], */
-                            'guide' => $guide,
-                        ]);
-            
-                        $pdf->setPaper('a4', 'landscape');
-                        
-                        return $pdf->download('guia_generada.pdf');//.$pdf->stream('guia_generada.pdf');
-                
-            }
-
-            /* return response()->json([
+            return response()->json([
                 // back()->with('success', 'Excel guides imported successfully')
                 'data' => 'success, Excel guides imported successfully',
-            ], 201); */
+            ], 201);
                 
         } catch (\Maatwebsite\Excel\Validators\ValidationException $e) {
             $failures = $e->failures();
