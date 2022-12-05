9c1a7bc75ad3fe0250cf178f5e49d0ff42aadfcb
Nicolas <nicolashilarion420@gmail.com>
2022-12-04 19:44:34 -0500

    configuring format pdf document

--- resources/views/pdf/generate.blade.php
+++ resources/views/pdf/generate.blade.php
@@ -4,56 +4,112 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Guía PDF</title>
 </head>
 <body>
     <header>
-        <h1 style="text-align: center"> Guia en PDF </h1>
-        <hr> <br>
-        <p style="text-align: center">{{ $guide->valor_declarado }}</p>
-        {{-- <p style="text-align: center">{{ $guide->tipoEnvio->nombre }}</p> --}}
-        <p style="text-align: center">{{ $guide->aplica_contrapago }}</p>
-        <p style="text-align: center">{{ $guide->peso_bruto }}</p>
-        <p style="text-align: center">{{ $guide->unidad }}</p>
-        <p style="text-align: center">{{ $guide->dice_contener }}</p>
-        <p style="text-align: center">{{ $guide->observaciones }}</p>
-        <p style="text-align: center">{{ $guide->shipping_pickup }}</p>
-        <p style="text-align: center">{{ $guide->urlguide }}</p>
-        <p style="text-align: center">{{ $guide->receiver->tipo_documento }}</p>
-        <p style="text-align: center">{{ $guide->receiver->numero_documento }}</p>
-        <p style="text-align: center">{{ $guide->receiver->nombre }}</p>
-        <p style="text-align: center">{{ $guide->receiver->primer_apellido }}</p>
-        <p style="text-align: center">{{ $guide->receiver->segundo_apellido }}</p>
-        <p style="text-align: center">{{ $guide->receiver->telefono }}</p>
-        <p style="text-align: center">{{ $guide->receiver->correo }}</p>
-        <p style="text-align: center">{{ $guide->receiver->direccion }}</p>
-        <p style="text-align: center">
-            {!! DNS1D::getBarcodeHTML($guide->id, 'EAN2') !!}
-        </p>
+        <div class="container py-12">
+
+            <div class="text-right">
+                <img class="h-12 w-12 rounded-full" src="proyecto2.jpeg" alt="Logo 99envios">
+            </div>
+
+            <div>
+                {!! DNS1D::getBarcodeHTML($guide->id, 'EAN2') !!}
+                {{$guide->id}}
+            </div>
+
+            <br>
+            <br>
+            {{-- <div class="grid grid-cols-2 gap-2 justify-center">
+                <div class="text-center inline-block">{{ $guide->valor_declarado }}</div>
+                <div class="text-center inline-block">{{ $guide->aplica_contrapago }}</div>
+                <div class="text-center inline-block">{{ $guide->peso_bruto }}</div>
+                <div class="text-center inline-block">{{ $guide->unidad }}</div>
+                <div class="text-center inline-block">{{ $guide->dice_contener }}</div>
+                <div class="text-center inline-block">{{ $guide->observaciones }}</div>
+                <div class="text-center inline-block">{{ $guide->shipping_pickup }}</div>
+                <div class="text-center inline-block">{{ $guide->urlguide }}</div>
+
+                <div class="text-center inline-block">{{ $guide->receiver->tipo_documento }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->numero_documento }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->nombre }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->primer_apellido }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->segundo_apellido }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->telefono }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->correo }}</div>
+                <div class="text-center inline-block">{{ $guide->receiver->direccion }}</div>
+            </div> --}}
+            <div class="justify-items-center">
+                <table class="table-auto">
+                    <thead>
+                        <tr>
+                            <th>Detalle del envío</th>
+                            <th>Receptor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $guide->valor_declarado }}</td>
+                            <td>{{ $guide->receiver->tipo_documento }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->aplica_contrapago }}</td>
+                            <td>{{ $guide->receiver->numero_documento }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->peso_bruto }}</td>
+                            <td>{{ $guide->receiver->nombre }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->unidad }}</td>
+                            <td>{{ $guide->receiver->primer_apellido }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->dice_contener }}</td>
+                            <td>{{ $guide->receiver->segundo_apellido }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->observaciones }}</td>
+                            <td>{{ $guide->receiver->telefono }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->shipping_pickup }}</td>
+                            <td>{{ $guide->receiver->correo }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ $guide->urlguide }}</td>
+                            <td>{{ $guide->receiver->direccion }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            </div>
 
 
-        <hr> <br>
-        {{-- <p style="text-align: center">{{ $guide->valor_declarado }}</p>
-        <p style="text-align: center">{{ $guide->tipoEnvio->nombre }}</p>
-        <p style="text-align: center">{{ $guide->aplica_contrapago }}</p>
-        <p style="text-align: center">{{ $guide->peso_bruto }}</p>
-        <p style="text-align: center">{{ $guide->unidad }}</p>
-        <p style="text-align: center">{{ $guide->dice_contener }}</p>
-        <p style="text-align: center">{{ $guide->observaciones }}</p>
-        <p style="text-align: center">{{ $guide->shipping_pickup }}</p>
-        <p style="text-align: center">{{ $guide->urlguide }}</p>
-        <p style="text-align: center">{{ $guide->receiver->tipo_documento }}</p>
-        <p style="text-align: center">{{ $guide->receiver->numero_documento }}</p>
-        <p style="text-align: center">{{ $guide->receiver->nombre }}</p>
-        <p style="text-align: center">{{ $guide->receiver->primer_apellido }}</p>
-        <p style="text-align: center">{{ $guide->receiver->segundo_apellido }}</p>
-        <p style="text-align: center">{{ $guide->receiver->telefono }}</p>
-        <p style="text-align: center">{{ $guide->receiver->correo }}</p>
-        <p style="text-align: center">{{ $guide->receiver->direccion }}</p> --}}
+                {{-- <hr> <br> --}}
+                {{-- <p style="text-align: center">{{ $guide->valor_declarado }}</p>
+                <p style="text-align: center">{{ $guide->aplica_contrapago }}</p>
+                <p style="text-align: center">{{ $guide->peso_bruto }}</p>
+                <p style="text-align: center">{{ $guide->unidad }}</p>
+                <p style="text-align: center">{{ $guide->dice_contener }}</p>
+                <p style="text-align: center">{{ $guide->observaciones }}</p>
+                <p style="text-align: center">{{ $guide->shipping_pickup }}</p>
+                <p style="text-align: center">{{ $guide->urlguide }}</p>
+                <p style="text-align: center">{{ $guide->receiver->tipo_documento }}</p>
+                <p style="text-align: center">{{ $guide->receiver->numero_documento }}</p>
+                <p style="text-align: center">{{ $guide->receiver->nombre }}</p>
+                <p style="text-align: center">{{ $guide->receiver->primer_apellido }}</p>
+                <p style="text-align: center">{{ $guide->receiver->segundo_apellido }}</p>
+                <p style="text-align: center">{{ $guide->receiver->telefono }}</p>
+                <p style="text-align: center">{{ $guide->receiver->correo }}</p>
+                <p style="text-align: center">{{ $guide->receiver->direccion }}</p> --}}
+
+            </div>
+        </div>
     </header>
-
-
-
-    
 </body>
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './storage/framework/views/*.php',
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
