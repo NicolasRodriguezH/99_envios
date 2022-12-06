14e125e46efcbeb4f1eeadfce4c0f04c68e9c168
Nicolas <nicolashilarion420@gmail.com>
2022-12-05 19:51:03 -0500

    sorting PDF elements

--- resources/views/pdf/generate.blade.php
+++ resources/views/pdf/generate.blade.php
@@ -9,107 +9,60 @@
     <title>Guía PDF</title>
 </head>
 <body>
-    <header>
-        <div class="container py-12">
-
+        <div class="container py-8">
             <div class="text-right">
-                <img class="h-12 w-12 rounded-full" src="proyecto2.jpeg" alt="Logo 99envios">
+                <img class="h-20 w-20 rounded-full" src="proyecto2.jpeg" alt="Logo 99envios">
             </div>
-
             <div>
                 {!! DNS1D::getBarcodeHTML($guide->id, 'EAN2') !!}
-                {{$guide->id}}
-            </div>
-
-            <br>
-            <br>
-            {{-- <div class="grid grid-cols-2 gap-2 justify-center">
-                <div class="text-center inline-block">{{ $guide->valor_declarado }}</div>
-                <div class="text-center inline-block">{{ $guide->aplica_contrapago }}</div>
-                <div class="text-center inline-block">{{ $guide->peso_bruto }}</div>
-                <div class="text-center inline-block">{{ $guide->unidad }}</div>
-                <div class="text-center inline-block">{{ $guide->dice_contener }}</div>
-                <div class="text-center inline-block">{{ $guide->observaciones }}</div>
-                <div class="text-center inline-block">{{ $guide->shipping_pickup }}</div>
-                <div class="text-center inline-block">{{ $guide->urlguide }}</div>
-
-                <div class="text-center inline-block">{{ $guide->receiver->tipo_documento }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->numero_documento }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->nombre }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->primer_apellido }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->segundo_apellido }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->telefono }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->correo }}</div>
-                <div class="text-center inline-block">{{ $guide->receiver->direccion }}</div>
-            </div> --}}
-            <div class="grid grid-cols-2 gap-4 place-content-around">
-                <table class="table-auto">
-                    <thead>
-                        <tr>
-                            <th>Detalle del envío</th>
-                            <th>Receptor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Valor declarado: {{ $guide->valor_declarado }}</td>
-                            <td>Tipo de documento: {{ $guide->receiver->tipo_documento }}</td>
-                        </tr>
-                        <tr>
-                            <td>Aplica contrapago: {{ $guide->aplica_contrapago }}</td>
-                            <td>Numero de documento: {{ $guide->receiver->numero_documento }}</td>
-                        </tr>
-                        <tr>
-                            <td>Peso bruto: {{ $guide->peso_bruto }}</td>
-                            <td>Nombre destinatario: {{ $guide->receiver->nombre }}</td>
-                        </tr>
-                        <tr>
-                            <td>Unidades: {{ $guide->unidad }}</td>
-                            <td>Primer apellido: {{ $guide->receiver->primer_apellido }}</td>
-                        </tr>
-                        <tr>
-                            <td>Dice contener: {{ $guide->dice_contener }}</td>
-                            <td>Segundo apellido: {{ $guide->receiver->segundo_apellido }}</td>
-                        </tr>
-                        <tr>
-                            <td>Recogida de envio: {{ $guide->shipping_pickup }}</td>
-                            <td>Telefono: {{ $guide->receiver->telefono }}</td>
-                        </tr>
-                        <tr>
-                            <td>Guia No: {{ $guide->id }}</td>
-                            <td>Correo: {{ $guide->receiver->correo }}</td>
-                        </tr>
-                        <tr>
-                            <td>Observaciones: {{ $guide->observaciones }}</td>
-                            <td>Direccion: {{ $guide->receiver->direccion }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            </div>
-
-
-                {{-- <hr> <br> --}}
-                {{-- <p style="text-align: center">{{ $guide->valor_declarado }}</p>
-                <p style="text-align: center">{{ $guide->aplica_contrapago }}</p>
-                <p style="text-align: center">{{ $guide->peso_bruto }}</p>
-                <p style="text-align: center">{{ $guide->unidad }}</p>
-                <p style="text-align: center">{{ $guide->dice_contener }}</p>
-                <p style="text-align: center">{{ $guide->observaciones }}</p>
-                <p style="text-align: center">{{ $guide->shipping_pickup }}</p>
-                <p style="text-align: center">{{ $guide->urlguide }}</p>
-                <p style="text-align: center">{{ $guide->receiver->tipo_documento }}</p>
-                <p style="text-align: center">{{ $guide->receiver->numero_documento }}</p>
-                <p style="text-align: center">{{ $guide->receiver->nombre }}</p>
-                <p style="text-align: center">{{ $guide->receiver->primer_apellido }}</p>
-                <p style="text-align: center">{{ $guide->receiver->segundo_apellido }}</p>
-                <p style="text-align: center">{{ $guide->receiver->telefono }}</p>
-                <p style="text-align: center">{{ $guide->receiver->correo }}</p>
-                <p style="text-align: center">{{ $guide->receiver->direccion }}</p> --}}
-
+                {!! $guide->id !!}
             </div>
         </div>
-    </header>
+
+        <div>
+            <table class="w-full border-separate">
+                <thead>
+                    <tr>
+                        <th class="border border-gray-400 py-4 px-4 text-gray-800 text-2xl">Detalle del envío</th>
+                        <th class="border border-gray-400 py-4 px-4 text-gray-800 text-2xl">Receptor</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr class="bg-gray-500">
+                        <td class="border border-gray-400 py-2 px-2 text-base">Valor declarado: {{ $guide->valor_declarado }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Tipo de documento: {{ $guide->receiver->tipo_documento }}</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Aplica contrapago: {{ $guide->aplica_contrapago }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Numero de documento: {{ $guide->receiver->numero_documento }}</td>
+                    </tr>
+                    <tr class="bg-gray-500">
+                        <td class="border border-gray-400 py-2 px-2 text-base">Peso bruto: {{ $guide->peso_bruto }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Nombre destinatario: {{ $guide->receiver->nombre }}</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Unidades: {{ $guide->unidad }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Primer apellido: {{ $guide->receiver->primer_apellido }}</td>
+                    </tr>
+                    <tr class="bg-gray-500">
+                        <td class="border border-gray-400 py-2 px-2 text-base">Dice contener: {{ $guide->dice_contener }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Segundo apellido: {{ $guide->receiver->segundo_apellido }}</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Recogida de envio: {{ $guide->shipping_pickup }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Telefono: {{ $guide->receiver->telefono }}</td>
+                    </tr>
+                    <tr class="bg-gray-500">
+                        <td class="border border-gray-400 py-2 px-2 text-base">Guia No: {{ $guide->id }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Correo: {{ $guide->receiver->correo }}</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Observaciones: {{ $guide->observaciones }}</td>
+                        <td class="border border-gray-400 py-2 px-2 text-base">Direccion: {{ $guide->receiver->direccion }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 </body>
 </html>
